05e2d22ce0295ce50b43e42bf7728410572512e8
pera1997 <mpetar97@gmail.com>
2022-02-21 23:17:34 +0100

    "inicijalni komit"

---
+++ src/main/java/com/poslovna/Poslovna/PoslovnaApplication.java
@@ -0,0 +1,13 @@
+package com.poslovna.Poslovna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoslovnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoslovnaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poslovna/Poslovna/PoslovnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poslovna.Poslovna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoslovnaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
